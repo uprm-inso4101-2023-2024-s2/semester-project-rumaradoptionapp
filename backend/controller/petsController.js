38863e67eb4693a74ded24f49704977288d1190a
Alej24 <alejandro.rivera24@upr.edu>
2024-04-09 18:59:20 -0400

    postPetRegistration fixed

--- backend/controller/petsController.js
+++ backend/controller/petsController.js
@@ -3,8 +3,14 @@ const petsDao = require('../dao/Pets');
 const petRegistration = async (req, res) => {
   try {
     const pet = req.body;
-    const data = await petsDao.addPet(pet);
-    res.json(data); // Send back the registered pet details
+    if (!pet.name || !pet.age || !pet.species || !pet.weight || !pet.size || !pet.personality || !pet.sex) {
+        // If any or all fields are missing, do not register the pet
+        res.redirect('/postPetRegistration?registered=false');
+    } else {
+        // If all fields on the perRegistration form are filled, register the pet
+        const data = await petsDao.addPet(pet);
+        res.redirect('/postPetRegistration?registered=true');
+    }
   } catch (error) {
     res.status(500).json({ error: error.message }); // Send an error message
   }
